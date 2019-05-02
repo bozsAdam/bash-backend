b961b188c25d03bc79b4400a35789553ea7b1ef3
Adam Bozsoki <09.adam.b@gmail.com>
2019-05-02 10:34:37 +0200

    Initial commit

---
+++ src/main/java/hu/adam/bash/BashApplication.java
@@ -0,0 +1,13 @@
+package hu.adam.bash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BashApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BashApplication.class, args);
+    }
+
+}
